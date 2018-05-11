e46e94b8c99d0b7feeb9bb84d3de3ec2a038a0a5
Niranjan <niranjan.518@gmail.com>
2018-05-11 22:32:46 +0200

    changes

--- src/main/java/com/tmt/family/FamilyApplicationLauncher.java
+++ src/main/java/com/tmt/family/FamilyApplicationLauncher.java
@@ -2,17 +2,13 @@ package com.tmt.family;
 
 import com.tmt.family.action.Action;
 import com.tmt.family.entity.Family;
-import com.tmt.family.entity.Person;
-import com.tmt.family.enums.GenderType;
 import com.tmt.family.parser.InputParser;
-
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.io.StringReader;
 import java.util.Scanner;
 
 public class FamilyApplicationLauncher {
-    public static void main(String arg[])  {
+
+    public static void main(String arg[]) {
         FamilyApplicationLauncher launcher = new FamilyApplicationLauncher();
         launcher.launch();
 
@@ -24,25 +20,26 @@ public class FamilyApplicationLauncher {
         Family family = FamilyFactory.getDefaultFamily();
         FamilyRelationManager familyRelationManager = new FamilyRelationManager(family);
 
-
         System.out.print("Input : ");
-        InputStream is = FamilyApplicationLauncher.class.getClassLoader().getResourceAsStream("default-family-members.txt");
+        InputStream is = FamilyApplicationLauncher.class.getClassLoader()
+                .getResourceAsStream("default-family-members.txt");
+
         Scanner sc = new Scanner(is);
-        acceptAndProcess(sc,familyRelationManager);
+        acceptAndProcess(sc, familyRelationManager);
         sc.close();
 
         sc = new Scanner(System.in);
-        acceptAndProcess(sc,familyRelationManager);
+        acceptAndProcess(sc, familyRelationManager);
 
     }
 
     private void acceptAndProcess(Scanner sc, FamilyRelationManager familyRelationManager) {
         String input;
-        while (!(input = sc.nextLine()).equalsIgnoreCase("EXIT")) {
-            try{
+        while (sc.hasNextLine() && !(input = sc.nextLine()).equalsIgnoreCase("EXIT")) {
+            try {
                 Action action = InputParser.parse(input);
                 familyRelationManager.handleRelation(action);
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
             System.out.print("Input : ");
